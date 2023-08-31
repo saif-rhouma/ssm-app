018dfd84db659e59bdce71b08586b365f7a0c994
Saifeddine Rhouma <saifeddine.rhouma.dev@gmail.com>
2023-08-31 21:20:09 +0100

    fix exception msg

--- src/app/middleware/authMiddleware.js
+++ src/app/middleware/authMiddleware.js
@@ -2,6 +2,7 @@ import environment from '../../config/environment';
 import logger from '../../utils/logger';
 import SSMToken from '../../utils/ssmToken';
 import InvalidTokenException from '../exceptions/unvalidTokenException';
+import messages from '../constants/messages';
 
 const authMiddleware = (req, res, next) => {
   if (environment.isDev) {
@@ -13,7 +14,7 @@ const authMiddleware = (req, res, next) => {
   if (SSMToken.validate(token)) {
     next();
   } else {
-    throw new InvalidTokenException();
+    throw new InvalidTokenException(messages.exceptions.invalidTokenException);
   }
 };
 export default authMiddleware;
